f68cbf8d0490197f5e358a34e992d6d7d685c967
Cache Makers <Image15@iMac-AA.local>
2017-03-07 18:00:26 -0700

    I add a hero graphic

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #define WIDE 800
 #define HIGH 600
 # include <iostream>
+#define TILESIZE 96
+
+
+
+
+
+
+
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"ADVENTURE OF GERALD THE SOLDIER");
+  sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"ADVENTURE OF GERALD THE ROBBER");
 
 
 
@@ -13,11 +21,19 @@ int main()
   circle.setFillColor(sf::Color::Green);
   circle.setPosition(200,200);
 //Make Hero
-  sf::RectangleShape hero(sf::Vector2f(50,50));
-  hero.setFillColor(sf::Color::Red);
-  hero.setPosition(WIDE-50,0);
+  sf::Texture hero_texture;
+  if(!hero_texture.loadFromFile("Assets/Hero.png"))
+  {
+    std::cout << "Error loading texture" << std::endl;
+  }
+  sf::Sprite hero;
+  hero.setTexture(hero_texture);
+
+  hero.setScale(sf::Vector2f(3,3));
+
+
   int circleDirection = 0;
-  int speed = 1;
+  double speed = 1;
   int points = 10;
 
   //Game loop
@@ -38,7 +54,7 @@ int main()
     {
       hero.move(0,-speed);
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y<HIGH-hero.getSize().y)
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y<HIGH-TILESIZE)
     {
       hero.move(0,speed);
     }
@@ -46,12 +62,13 @@ int main()
     {
       hero.move(-speed,0);
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x<WIDE-hero.getSize().x)
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x<WIDE-TILESIZE)
     {
       hero.move(speed,0);
     }
 
     if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+    {
 
       std::cout << "                      ****************************************                                         Touching circle" << std::endl;
     }
