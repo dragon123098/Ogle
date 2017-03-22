f511c233a1324cff789af272bbedef7ebb4ce443
Cache Makers <Image15@iMac-AA.local>
2017-03-21 18:05:31 -0600

    added more walls

--- main.cpp
+++ main.cpp
@@ -1,32 +1,93 @@
 # include <SFML/Graphics.hpp>
 #define WIDE 800
-#define HIGH 600
+#define HIGH 640
 # include <iostream>
-#define TILESIZE 96
+#define TILESIZE 32
+#include <vector>
 
 
 
+bool testCall(sf::Sprite a, sf::Sprite b)
+{
+  return a.getGlobalBounds().intersects(b.getGlobalBounds());
+}
 
-
+bool testWallColl(sf::Sprite a, std::vector<sf::Sprite>walls)
+{
+  for(int i=0; i<walls.size(); i++)
+  {
+    if(a.getGlobalBounds().intersects(walls[i].getGlobalBounds()))
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
 
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"ADVENTURE OF GERALD THE ROBBER");
+  int tileW = WIDE/TILESIZE;
+  int tileH = HIGH/TILESIZE;
+  std::vector<std::vector<bool> > map;
+
+  for(int i=0; i<tileH; i ++)
+  {
+    std::vector<bool> temp;
+    for(int j=0; j<tileW; j++)
+    {
+      temp.push_back(false);
+    }
+    map.push_back(temp);
+  }
 
 
+  map[3][2] = true;
+  map[3][3] = true;
+  map[3][4] = true;
+  map[2][10] = true;
+  for(int i=0; i<map.size(); i++)
+  {
+      for(int j=0; j<map[i].size(); j++)
+      {
+        if(map[i][j] == true)
+        {
+          std::cout << '0';
+        }
+        else
+        {
+          std::cout << ' ';
+        }
+      }
+      std::cout << std::endl;
+  }
 
-//Make Enemy/Circle
-sf::Texture circle_texture;
-if(!circle_texture.loadFromFile("Assets/EnemyDog.png"))
+
+//Make Enemy/enemy
+sf::Texture enemy_texture;
+if(!enemy_texture.loadFromFile("Assets/EnemyDog.png"))
 {
   std::cout << "Error loading texture" << std::endl;
 }
-sf::Sprite circle;
-circle.setTexture(circle_texture);
 
-circle.setScale(sf::Vector2f(5,5));
-circle.setPosition(100,100);
+//I know someone who is a dirty dishrag
+sf::Sprite enemy;
+enemy.setTexture(enemy_texture);
+
+enemy.setScale(sf::Vector2f(5,5));
+enemy.setPosition(100,100);
+
+//cloud
+sf::Texture cloud_texture;
+if(!cloud_texture.loadFromFile("Assets/grass.png"))
+{
+  std::cout << "thkgds;jafkldajs;" << std::endl;
+}
+sf::Sprite cloud;
+cloud.setTexture(cloud_texture);
+//He lives in Boston
+cloud.setScale(sf::Vector2f(2,2));
 
 
 
@@ -63,10 +124,24 @@ livesDisp.setColor(sf::Color::Blue);
     std::cout << "Error loading rock" << std::endl;
   }
 
-  sf::Sprite rock;
-  rock.setTexture(rock_texture);
-  rock.setScale(sf::Vector2f(3,3));
-  rock.setPosition(30,250);
+
+  //He likes to eat butterflies
+  std::vector<sf::Sprite> wallArr;
+
+    for(int i=0; i<map.size(); i++)
+    {
+        for(int j=0; j<map[i].size(); j++)
+        {
+          if(map[i][j] == true)
+          {
+            sf::Sprite temp;
+            temp.setTexture(rock_texture);
+            temp.setScale(sf::Vector2f(3,3));
+            temp.setPosition(i*TILESIZE,j*TILESIZE);
+            wallArr.push_back(temp);
+          }
+        }
+    }
 
 
 
@@ -76,7 +151,9 @@ livesDisp.setColor(sf::Color::Blue);
 
 
 
-  int circleDirection = 0;
+
+  int enemyDirection = 0;
+
 
 
   double speed = .7;
@@ -104,8 +181,8 @@ livesDisp.setColor(sf::Color::Blue);
     //Control movement
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
     {
-      hero.move(50,-speed);
-      if(hero.getGlobalBounds().intersects(rock.getGlobalBounds()))
+      hero.move(0,-speed);
+      if(testWallColl(hero, wallArr))
       {
         hero.move(0,speed);
       }
@@ -113,7 +190,7 @@ livesDisp.setColor(sf::Color::Blue);
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y<HIGH-TILESIZE)
     {
       hero.move(0,speed);
-      if(hero.getGlobalBounds().intersects(rock.getGlobalBounds()))
+      if(testWallColl(hero, wallArr))
       {
         hero.move(0,-speed);
       }
@@ -121,7 +198,7 @@ livesDisp.setColor(sf::Color::Blue);
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)&& hero.getPosition().x > 0)
     {
       hero.move(-speed,0);
-      if(hero.getGlobalBounds().intersects(rock.getGlobalBounds()))
+      if(testWallColl(hero, wallArr))
       {
         hero.move(speed,0);
       }
@@ -129,7 +206,7 @@ livesDisp.setColor(sf::Color::Blue);
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x<WIDE-TILESIZE)
     {
       hero.move(speed,0);
-      if(hero.getGlobalBounds().intersects(rock.getGlobalBounds()))
+      if(testWallColl(hero, wallArr))
       {
         hero.move(-speed,0);
       }
@@ -137,7 +214,7 @@ livesDisp.setColor(sf::Color::Blue);
 
 
     //Colide with Enemy
-    if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+    if(hero.getGlobalBounds().intersects(enemy.getGlobalBounds()))
     {
       if(takingDamage == false)
       {
@@ -155,32 +232,50 @@ livesDisp.setColor(sf::Color::Blue);
     livesDisp.setString(std::to_string(lives));
 
      window.clear(sf::Color::White);
-    //controls circle movment
-    if(circleDirection == 0)
+    //controls enemy movment
+    if(enemyDirection == 0)
     {
-      circle.move(1,0);
+      enemy.move(1,0);
     }
     else
     {
-      circle.move(-1,0);
+      enemy.move(-1,0);
     }
-    if(circle.getPosition().x > 600)
+    if(enemy.getPosition().x > 600)
     {
-      circleDirection = 1;
+      enemyDirection = 1;
     }
-    if(circle.getPosition().x < 0)
+    if(enemy.getPosition().x < 0)
     {
-      circleDirection = 0;
+      enemyDirection = 0;
     }
+
+    for(int i=0; i<tileH; i++)
+    {
+      for(int j=0; j<tileW; j++)
+      {
+        cloud.setPosition(j*TILESIZE, i*TILESIZE);
+        window.draw(cloud);
+      }
+    }
+
+
 
     //Draw Text
     window.draw(livesDisp);
     //Drow hero
     window.draw(hero);
-    //Draw Circle
-    window.draw(circle);
-    //Draw Rock
-    window.draw(rock);
+    //Draw enemy
+    window.draw(enemy);
+
+    for(int i=0; i<wallArr.size(); i++)
+    {
+      window.draw(wallArr[i]);
+    }
+    //Draw background
+
+
+
     //Display all stuff
     window.display();
 
